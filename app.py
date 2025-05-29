4183ce28f9e2d53be23e348f2d41c5122e1b295e
joyeeta9153 <142172256+joyeeta9153@users.noreply.github.com>
2025-05-29 21:39:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser ##default output parser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+load_dotenv() 
+open_api=os.getenv("OPEN_API_KEY")
+#langsmith
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+langchain_api=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant.Please respond to the user queries."),
+        ("user","Question:{question}")
+    ]
+)
+
+##streamlit framework
+
+st.title('Langchain Demo With OPENAI API')
+input_text=st.text_input("Search the topic you want")
+
+# openAI llm
+
+llm=ChatOpenAI(model_name="gpt-3.5-turbo",openai_api_key=open_api)
+output_parser=StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    try:
+        response = chain.invoke({'question': input_text})
+        st.write(response)
+    except Exception as e:
+        import traceback
+        st.error("An error occurred.")
+        st.code(traceback.format_exc())
